583d8845d5020a2bae9826f8a38d31238698f5a5
astroty <astroty.br@gmail.com>
2024-06-24 16:08:30 -0300

    QoL script adjust
    Personal server adjusts

--- rebuild.sh
+++ rebuild.sh
@@ -1,6 +1,5 @@
-export CFLAGS="-march=native -mtune=native -O3 -pipe -fno-plt -fexceptions -Wp,-D_FORTIFY_SOURCE=2 -Wformat -Werror=format-security -fstack-clash-protection -fcf-protection -fopenmp -pthread" 
+export CFLAGS="-march=native -mtune=native -ggdb -pipe -fno-plt -fexceptions -Wp,-D_FORTIFY_SOURCE=2 -Wformat -Werror=format-security -fstack-clash-protection -fcf-protection -fopenmp -pthread"
 export CXXFLAGS="$CFLAGS -Wp,-D_GLIBCXX_ASSERTIONS" 
 export LDFLAGS="-Wl,-O1,--sort-common,--as-needed,-z,relro,-z,now,-lgomp,-lpthread"
-./configure
 make clean
-make -j16 server
+make -j6 server
