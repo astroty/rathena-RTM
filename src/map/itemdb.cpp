1de2e1821c68fa7867399883083b47c7700d8fa3
Corax <do@not.me>
2025-04-21 20:44:37 +0200

    drop items identified

--- src/map/itemdb.cpp
+++ src/map/itemdb.cpp
@@ -1635,11 +1635,11 @@ bool itemdb_ishatched_egg(struct item* item) {
 char itemdb_isidentified(t_itemid nameid) {
 	int type=itemdb_type(nameid);
 	switch (type) {
-		case IT_WEAPON:
-		case IT_ARMOR:
-		case IT_PETARMOR:
-		case IT_SHADOWGEAR:
-			return 0;
+		//case IT_WEAPON:
+		//case IT_ARMOR:
+		//case IT_PETARMOR:
+		//case IT_SHADOWGEAR:
+		//	return 0;
 		default:
 			return 1;
 	}
